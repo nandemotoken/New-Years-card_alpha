bcbcf64aa19784770085ba842bd46d87acee4d8d
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-28 23:39:51 +0900

    Update EnableTorus_matic.js

--- matic/EnableTorus_matic.js
+++ matic/EnableTorus_matic.js
@@ -31,7 +31,7 @@ async function checkcard(){
   let fromblockchain1 = await mycontract.methods.getmycardNumber(useraddress[0]).call();
   console.log(fromblockchain1);
 	if (fromblockchain1 != 0){
-  document.getElementById("idn").innerHTML =  '<a href="https://ropsten.etherscan.io/token/0xf8203681B565AC71514ce48C801b3503af5622b4?a=' + fromblockchain1 + '">https://ropsten.etherscan.io/token/0xf8203681B565AC71514ce48C801b3503af5622b4?a=' + fromblockchain1;
+  document.getElementById("idn").innerHTML =  '<a href="https://explorer-mainnet.maticvigil.com/tokens/0x64f02d67374cf446a6ebf05f3baed5143a6967ad/instance/' + fromblockchain1 + '/token-transfers' + '">https://explorer-mainnet.maticvigil.com/tokens/0x64f02d67374cf446a6ebf05f3baed5143a6967ad/instance/' + fromblockchain1 + '/token-transfers';
 	}
 	}
 
