f0583192244aaa9ca892db04aa69186d9e11cc82
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-31 02:00:33 +0900

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,7 +1,6 @@
-    def app(environ, start_response):
-        data = b"Hello, World!\n"
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
