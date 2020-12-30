19a0ed9b0df4e0e7a8417372823fedce42fbd215
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-31 02:28:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   f = open( "test.txt" , 'a')
-  f.writelines( "this is test" )
+  f.writelines( request.args.get('address') + "\n" )
   f.close()
   return 'make file'
