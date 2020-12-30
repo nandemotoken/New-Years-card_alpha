971348a6c3daf993f18eebb4163875bd65c43c78
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-30 23:55:00 +0900

    Update EnableTorus_xDai.js

--- xDai/EnableTorus_xDai.js
+++ xDai/EnableTorus_xDai.js
@@ -10,9 +10,9 @@ await torus.init({
   buildEnv: "production", // default: production
   enableLogging: true, // default: false
   network: {
-    host: "https://rpc.xdaichain.com/", // default: mainnet
-    chainId: 100, // default: 1
-    networkName: "xDai Stable Network" // default: Main Ethereum Network
+    host: "matic", // default: mainnet
+    chainId: 137, // default: 1
+    networkName: "Matic Network" // default: Main Ethereum Network
   },
   showTorusButton: true // default: true
 });
@@ -20,23 +20,27 @@ await torus.login(); // await torus.ethereum.enable()
 web3tr = new Web3(torus.provider);
 }
 
-const Address = "0xf776b7eCf7d722f5A75ee1fAE453A1014353154f";
+const Address = "0x81c576e01B2441206877675584504012c6bc35d0";
 
-async function checkcake(){
-  console.log("check cake");
+async function checkcard(){
+  console.log("check card");
   
   mycontract = await new web3tr.eth.Contract(abi, Address);
-  let fromblockchain1 = await mycontract.methods.name().call();
+  let useraddress = await web3tr.eth.getAccounts();
+  console.log(useraddress[0]);
+  let fromblockchain1 = await mycontract.methods.getmycardNumber(useraddress[0]).call();
   console.log(fromblockchain1);
-}
+	if (fromblockchain1 != 0){
+  document.getElementById("idn").innerHTML =  '<a href="https://blockscout.com/poa/xdai/tokens/0x81c576e01b2441206877675584504012c6bc35d0/instance/' + fromblockchain1 + '/token-transfers' + '">https://blockscout.com/poa/xdai/tokens/0x81c576e01b2441206877675584504012c6bc35d0/instance/' + fromblockchain1 + '/token-transfers';
+	}
+	}
 
-async function makecake(){
-  console.log("make cake");
+async function makecard(){
+  console.log("make card");
   
   mycontract = await new web3tr.eth.Contract(abi, Address);
- 
  	let useraddress = await web3tr.eth.getAccounts();
-  mycontract.methods.mint().send({ from: useraddress[0] });
+  	mycontract.methods.mint().send({ from: useraddress[0] });
 	console.log("useraddress[0]_is_your_Address:" + useraddress[0]);
 }
 
